045b3c02ac2cdc2d85e70504ae178c0736a9813f
Mallory Wittwer <mallory.wittwer@epfl.ch>
2025-01-08 17:41:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+![EPFL Center for Imaging logo](https://imaging.epfl.ch/resources/logo-for-gitlab.svg)
+# serverkit-orientationpy
+
+Implementation of a web server for [Orientationpy](https://gitlab.com/epfl-center-for-imaging/orientationpy).
+
+Author: EPFL Center for Imaging
+
+## Installing the algorithm server with `pip`
+
+Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Run the server:
+
+```
+uvicorn main:app --host 0.0.0.0 --port 8000
+```
+
+The server will be running on http://localhost:8000.
+
+## Endpoints
+
+A documentation of the endpoints is automatically generated at http://localhost:8000/docs.
+
+**GET endpoints**
+
+- http://localhost:8000/: Listing of running algorithm servers.
+- http://localhost:8000/version: Version of the `imaging-server-kit` package.
+- http://localhost:8000/orientationpy/info: Web page displaying project metadata.
+- http://localhost:8000/orientationpy/demo: Plotly Dash web demo app.
+- http://localhost:8000/orientationpy/parameters: Json Schema representation of algorithm parameters.
+- http://localhost:8000/orientationpy/sample_images: Bytes string representation of the sample images.
+
+**POST endpoints**
+
+- http://localhost:8000/orientationpy/: Processing endpoint to run the algorithm.
+
+## Running the server with `docker-compose`
+
+To build the docker image and run a container for the algorithm server in a single command, use:
+
+```
+docker compose up
+```
+
+The server will be running on http://localhost:8000.
+
+## Running the server with `docker`
+
+Build the docker image:
+
+```
+docker build -t serverkit-orientationpy .
+```
+
+Run the server in a container:
+
+```
+docker run -it --rm -p 8000:8000 serverkit-orientationpy:latest
+```
+
+The server will be running on http://localhost:8000.
+
+## Running unit tests
+
+If you have implemented unit tests in the [tests/](./tests/) folder, you can run them using pytest:
+
+```
+pytest
+```
+
+if you are developing your server locally, or
+
+```
+docker run --rm serverkit-orientationpy:latest pytest
+```
+
+to run the tests in a docker container.
+
+## [EPFL only] Publishing a docker image to [registry.rcp.epfl.ch](https://registry.rcp.epfl.ch/)
+
+```
+docker tag serverkit-orientationpy registry.rcp.epfl.ch/imaging-server-kit/serverkit-orientationpy
+docker push registry.rcp.epfl.ch/imaging-server-kit/serverkit-orientationpy
+```
+
+## Sample images provenance
+
+- `image1_from_OrientationJ.tif`: Example image from [OrientationJ](https://bigwww.epfl.ch/demo/orientationj/).
---
+++ main.py
@@ -0,0 +1,168 @@
+from typing import List, Literal, Type
+from pathlib import Path
+import numpy as np
+from pydantic import BaseModel, Field, field_validator
+import uvicorn
+import skimage.io
+import imaging_server_kit as serverkit
+import orientationpy
+import matplotlib
+
+
+def rescale_intensity_quantile(image):
+    """Rescale the image intensity based on the 2nd and 98th quantiles."""
+    image = image.astype(np.float64)
+    image_normed = image - np.quantile(image, 0.02)
+    image_normed = image / np.quantile(image_normed, 0.98)
+    return image_normed
+
+
+class Parameters(BaseModel):
+    """Defines the algorithm parameters"""
+    image: str = Field(
+        ...,
+        title="Image",
+        description="Input image (2D, 3D).",
+        json_schema_extra={"widget_type": "image"},
+    )
+    mode: Literal["fiber", "membrane"] = Field(
+        default="fiber",
+        title="Mode",
+        description="The orientation computation mode.",
+        json_schema_extra={"widget_type": "dropdown"},
+    )
+    scale: float = Field(
+        default=1.0,
+        title="Structural scale",
+        description="The scale at which orientation is computed.",
+        ge=0.1,
+        le=10.0,
+        json_schema_extra={
+            "widget_type": "float",
+            "step": 1.0,
+        },
+    )
+    with_colors: bool = Field(
+        default=False,
+        title="Output color-coded orientation",
+        description="Whether to output a color-coded representation of orientation or not.",
+        json_schema_extra={"widget_type": "bool"},
+    )
+    vector_spacing: int = Field(
+        default=1,
+        title="Vector spacing",
+        description="The spacing at which the orientation vectors are rendered.",
+        ge=1,
+        le=10,
+        json_schema_extra={
+            "widget_type": "int",
+            "step": 1,
+        },
+    )
+    @field_validator("image", mode="after")
+    def decode_image_array(cls, v) -> np.ndarray:
+        image_array = serverkit.decode_contents(v)
+        if image_array.ndim not in [2, 3]:
+            raise ValueError("Array has the wrong dimensionality.")
+        return image_array
+
+# Define the run_algorithm() method for your algorithm
+class Server(serverkit.Server):
+    def __init__(
+        self,
+        algorithm_name: str="orientationpy",
+        parameters_model: Type[BaseModel]=Parameters
+    ):
+        super().__init__(algorithm_name, parameters_model)
+
+    def run_algorithm(
+        self,
+        image: np.ndarray,
+        mode: str,
+        scale: float,
+        with_colors: bool,
+        vector_spacing: int,
+        **kwargs,
+    ) -> List[tuple]:
+        """Run the orientationpy algorithm."""
+        if image.ndim == 2:
+            mode = "fiber"  # no membranes in 2D
+
+        gradients = orientationpy.computeGradient(image, mode="splines")
+        structureTensor = orientationpy.computeStructureTensor(gradients, sigma=scale)
+        orientation_returns = orientationpy.computeOrientation(
+            structureTensor,
+            mode=mode,
+            computeEnergy=False,
+            computeCoherency=True,
+        )
+        theta = orientation_returns.get("theta") + 90
+        phi = orientation_returns.get("phi")
+        coherency = rescale_intensity_quantile(orientation_returns.get("coherency"))
+
+        boxVectorCoords = orientationpy.anglesToVectors(orientation_returns)
+
+        # This is messy... but it works (kinda)!
+        node_spacings = np.array([vector_spacing] * image.ndim).astype(int)
+        slices = [slice(n // 2, None, n) for n in node_spacings]
+        grid = np.mgrid[[slice(0, x) for x in image.shape]]
+        node_origins = np.stack([g[tuple(slices)] for g in grid])
+        slices.insert(0, slice(len(boxVectorCoords)))
+        displacements = boxVectorCoords[tuple(slices)].copy()
+        displacements *= np.mean(node_spacings)
+        displacements = np.reshape(displacements, (image.ndim, -1)).T
+        origins = np.reshape(node_origins, (image.ndim, -1)).T
+        origins = origins - displacements / 2
+        displacement_vectors = np.stack((origins, displacements))
+        displacement_vectors = np.rollaxis(displacement_vectors, 1)
+
+        data_tuple = [
+            (
+                displacement_vectors,
+                {
+                    "name": "Orientation vectors",
+                    "edge_width": np.max(node_spacings) / 5.0,
+                    "opacity": 1.0,
+                    "ndim": image.ndim,
+                    "edge_color": "blue",
+                    "vector_style": "line",
+                },
+                "vectors",
+            )
+        ]
+
+        if with_colors:
+            if image.ndim == 3:
+                imDisplayHSV = np.stack(
+                    (phi / 360, np.sin(np.deg2rad(theta)), image / image.max()), axis=-1
+                )
+            else:
+                imDisplayHSV = np.stack(
+                    (theta / 180, coherency, image / image.max()), axis=-1
+                )
+            imdisplay_rgb = matplotlib.colors.hsv_to_rgb(imDisplayHSV)
+
+            data_tuple.append(
+                (
+                    imdisplay_rgb,
+                    {
+                        "name": "Color-coded orientation",
+                        "rgb": True,
+                    },
+                    "image",
+                )
+            )
+
+        return data_tuple
+
+    def load_sample_images(self) -> List["np.ndarray"]:
+        """Load one or multiple sample images."""
+        image_dir = Path(__file__).parent / "sample_images"
+        images = [skimage.io.imread(image_path) for image_path in image_dir.glob("*")]
+        return images
+
+server = Server()
+app = server.app
+
+if __name__=='__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
